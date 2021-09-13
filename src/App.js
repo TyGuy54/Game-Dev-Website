ba646d994d635faa2e71e1d1d287f57ddfcb1a63
Tyler <TylerRoss107@gmail.com>
2021-09-13 08:04:18 -0400

    added another part to the web page, you can scroll down now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
-import Navbar from './components/Navebar';
+import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
+import Home from './pages';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Home />
     </Router>
   );
 }
