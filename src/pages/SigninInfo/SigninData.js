4a1eadb66c060ef6a4330f595f99b9662d989697
Tyler <TylerRoss107@gmail.com>
2021-09-21 12:22:06 -0400

    styled my login page, still need to style the input part

--- src/pages/SigninInfo/SigninData.js
+++ src/pages/SigninInfo/SigninData.js
@@ -3,22 +3,58 @@ import styled from "styled-components";
 export const SigninContainer = styled.div`
     color: #fff;
     background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '#010606')};
-    margin-top: 210px;
+    margin-top: 0px;
+    padding-top: 0px;
+    padding-bottom: 107px;
 
     @media screen and ( max-width: 768px) {
         padding: 100px 0;
-        margin-top: 100px
+        margin-top: 0px;
+        padding-top: 100px;
+        padding-bottom: 500px;
     }
 
 `;
 
-export const SignInName = styled.h1`
+export const TyguyLogo = styled.h1`
+    text-align: center;
+    font-weight: bold;
+`;
+
+export const SignInForom = styled.div`
+    border-radius: 40px;
+    background-color: #000;
+    padding: 100px;
+    box-shadow: 0px 0px 50px #33a0ff;
+    margin-top: 30px;
+    margin-bottom: 50px;
+    margin-left: 200px;
+    margin-right: 200px;
+`;
+
+// export const TestBox = styled.div`
+// box-shadow: 0px 0px 50px #33a0ff inset;
+// `;
+
+export const SignInName = styled.div`
     display: grid;
     z-index: 1;
     width: 100%
     max-width: 1100px;
     margin-right: auto;
     margin-left: auto;
+    padding: 0 24px;
+    justify-content: center;
+`;
+
+export const SignInPass = styled.div`
+    display: grid;
+    z-index: 1;
+    width: 100%
+    max-width: 1100px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 70px;
     padding: 0 24px;
     justify-content: center;
 `;
@@ -41,6 +77,6 @@ export const SignInButton = styled.button`
 
     &:hover{
         transition: all 0.2s ease-in-out;
-        background: ${({primary}) => (primary ? '#fff' :'red')};
+        background: ${({primary}) => (primary ? '#fff' :'#fff')};
     }
 `;
--- src/pages/SigninInfo/SigninPage.js
+++ src/pages/SigninInfo/SigninPage.js
@@ -4,26 +4,37 @@ import FirebaseUser from '../FirebaseUser';
 import {
     SignInName,
     SignInButton,
-    SigninContainer
+    SigninContainer,
+    SignInPass,
+    TyguyLogo,
+    SignInForom,
+    //TestBox
 } from "./SigninData"
 
 const SigninPage = () => {
     const [ signInOrUp, setSignInOrUp ] = useState("");
     return (
       <SigninContainer>
-      <SignInName>
-      {
-        signInOrUp ? (
-          <FirebaseUser action={ signInOrUp } />
-        ) : (
-          <>
-            <SignInButton type="button" onClick={ () => setSignInOrUp("signin") } >Log in with existing user</SignInButton>
-            <SignInButton type="button" onClick={ () => setSignInOrUp("signup") } >Create a new user</SignInButton>
-          </>
-        )
-      }
-      </SignInName>
-    </SigninContainer>
+        <div>
+          {
+            signInOrUp ? (
+              <FirebaseUser action={ signInOrUp } />
+            ) : (
+              <>
+                <TyguyLogo>Tyguy Studios</TyguyLogo>
+                  <SignInForom>
+                    <SignInName>
+                      <SignInButton type="button" onClick={ () => setSignInOrUp("signin") } >Log in with existing user</SignInButton>
+                    </SignInName>
+                    <SignInPass>
+                      <SignInButton type="button" onClick={ () => setSignInOrUp("signup") } >Create a new user</SignInButton>
+                    </SignInPass>
+                  </SignInForom>
+              </>
+            )
+          }
+        </div>
+      </SigninContainer>
     );
 }
 
